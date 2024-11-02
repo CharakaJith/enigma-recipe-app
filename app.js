3e5b3d4a637a3e0e2804033008ebb72886add3dc
Charaka Jith Gunasinghe <charaka.20200241@iit.ac.lk>
2024-11-02 13:08:53 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const env = process.env.NODE_ENV || 'development';
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(
+    chalk.white.bgGreen.bold(' PORT ') +
+      chalk.white.bgBlue.bold(` ${PORT} `) +
+      chalk.white.bgGreen.bold(' MODE ') +
+      chalk.white.bgRed.bold(` ${env} `)
+  );
+});
